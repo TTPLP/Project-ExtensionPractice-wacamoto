5725c80da9b019357ddd6ae02bf615cdabe1b3bc
waka <tavukizartma@gmail.com>
2016-01-28 15:02:08 +0800

    fix background.js 'split' error

--- js/function.js
+++ js/function.js
@@ -22,19 +22,20 @@ function getToken() {
     chrome.tabs.query({
         active: true
     }, function(tabs) {
-        tabid = tabs[0].id;
-        chrome.tabs.onUpdated.addListener(function(tabid, tab) {
-            var tabUrl = tab.url;
-            var params = tabUrl.split('#')[1];
-            var accessToken = params.split('&')[0];
-            var expiresTime = params.split('&')[1];
-            expiresTime = expiresTime.split('=')[1];
-            localStorage.accessToken = accessToken.split('=')[1];
-            localStorage.expiresTime = Date.now() + Number(expiresTime)*1000;
-            
-            // get 60 day accessToken
-            getLongLiveToken(localStorage.accessToken)
-        })
+        for (var i = 0; i < tabs.length; i++) {
+            if (tabs[i].url.indexOf(app.redirect_uri) !== -1) {
+                var tabUrl = tabs[i].url;
+                var params = tabUrl.split('#')[1];
+                var accessToken = params.split('&')[0];
+                var expiresTime = params.split('&')[1];
+                expiresTime = expiresTime.split('=')[1];
+                localStorage.accessToken = accessToken.split('=')[1];
+                localStorage.expiresTime = Date.now() + Number(expiresTime)*1000;
+                
+                // get 60 day accessToken
+                getLongLiveToken(localStorage.accessToken)    
+            }
+        }
     })
 }
 
