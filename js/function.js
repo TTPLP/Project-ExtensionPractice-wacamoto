361cac043404f99347a13431dc86a9cd64a49bb1
waka <tavukizartma@gmail.com>
2016-01-28 14:45:40 +0800

    add background.html & function.js

---
+++ js/function.js
@@ -0,0 +1,58 @@
+app = {
+    id:             1674115332869324,
+    scope:          'publish_actions,publish_actions',
+    client_secret:  '3c4181b5d7db9c166d38dbd6773d52f6',
+    redirect_uri:   'http://www.facebook.com/connect/login_success.html',
+}
+
+function loginURL(site, path, params) {
+    var urlpar = '';
+    if (params) {
+        var keys = Object.keys(params);
+        for (var i = 0; i < keys.length; i++) {
+            urlpar += keys[i] + '=' + params[keys[i]] + '&'
+        }
+        urlpar = '?' + urlpar 
+    }
+    return 'https://'.concat(site, path, urlpar)
+}
+
+// get facebook accessToken
+function getToken() {
+    chrome.tabs.query({
+        active: true
+    }, function(tabs) {
+        tabid = tabs[0].id;
+        chrome.tabs.onUpdated.addListener(function(tabid, tab) {
+            var tabUrl = tab.url;
+            var params = tabUrl.split('#')[1];
+            var accessToken = params.split('&')[0];
+            var expiresTime = params.split('&')[1];
+            expiresTime = expiresTime.split('=')[1];
+            localStorage.accessToken = accessToken.split('=')[1];
+            localStorage.expiresTime = Date.now() + Number(expiresTime)*1000;
+            
+            // get 60 day accessToken
+            getLongLiveToken(localStorage.accessToken)
+        })
+    })
+}
+
+// get 60 day expiresTime accessToken 
+function getLongLiveToken(accessToken) {
+    $.ajax({
+        type: 'GET',
+        url: loginURL('graph.facebook.com', '/oauth/access_token', {
+            client_id:          app.id,
+            client_secret:      app.client_secret,
+            grant_type:         'fb_exchange_token',
+            fb_exchange_token:  localStorage.accessToken
+        })
+    }).done(function(response) {
+        var accessToken = response.split('&')[0];
+        var expiresTime = response.split('&')[1];
+        expiresTime = expiresTime.split('=')[1];
+        localStorage.accessToken = accessToken.split('=')[1];
+        localStorage.expiresTime = Date.now() + Number(expiresTime)*1000;
+    })
+}
--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,3 @@
-app = {
-    id:             1674115332869324,
-    scope:          'publish_actions,publish_actions',
-    client_secret:  '3c4181b5d7db9c166d38dbd6773d52f6',
-    redirect_uri:   'http://www.facebook.com/connect/login_success.html',
-}
-
 window.fbAsyncInit = function init() {
     chrome.tabs.query({
         active: true,
@@ -22,43 +15,9 @@ window.fbAsyncInit = function init() {
                 var img = $('<img>', {src: response.data.url})
                 $('body').append(img)
             })
-            // test 
-            getLongLiveToken(localStorage.accessToken)
         } else {
             loginFacebook()
         }    
-    })
-}
-
-// get 60 day expiresTime accessToken 
-function getLongLiveToken(accessToken) {
-    // FB.api('/oauth/access_token', 'GET', {
-    //     client_id:          '1674115332869324',
-    //     client_secret:      '3c4181b5d7db9c166d38dbd6773d52f6',
-    //     grant_type:         'fb_exchange_token',
-    //     fb_exchange_token:  localStorage.accessToken
-    // }, function (response) {
-    //     console.log(response)
-    //     console.log(
-    //         'access_token:' + response.access_token + 
-    //         'expires_in:' + response.expires_in
-    //     )
-    // })
-    $.ajax({
-        type: 'GET',
-        url: loginURL('graph.facebook.com', '/oauth/access_token', {
-            client_id:          app.id,
-            client_secret:      app.client_secret,
-            grant_type:         'fb_exchange_token',
-            fb_exchange_token:  localStorage.accessToken
-        })
-    }).done(function(response) {
-        var accessToken = response.split('&')[0];
-        var expiresTime = response.split('&')[1];
-        expiresTime = expiresTime.split('=')[1];
-        console.log(expiresTime)
-        localStorage.accessToken = accessToken.split('=')[1];
-        localStorage.expiresTime = Date.now() + Number(expiresTime)*1000;
     })
 }
 
@@ -73,20 +32,6 @@ function loginFacebook() {
     })
 }
 
-function loginURL(site, path, params) {
-    var urlpar = '';
-    if (params) {
-        var keys = Object.keys(params);
-        for (var i = 0; i < keys.length; i++) {
-            urlpar += keys[i] + '=' + params[keys[i]] + '&'
-        }
-        urlpar = '?' + urlpar 
-    }
-    return 'https://'.concat(site, path, urlpar)
-}
-
-
-
 (function(d, s, id){
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) {return;}
@@ -94,6 +39,3 @@ function loginURL(site, path, params) {
     js.src = "https://connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-
-
-
